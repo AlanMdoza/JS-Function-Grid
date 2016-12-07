8b641ac1c304dea21bd02a3af775f6264efbac30
Alanmendozauag <alanmendoza@uagateway.org>
2016-12-07 21:13:46 +0000

    added colors on the grid

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //Write your function calls below this line
+for (var i = 0; i< 20; i++){
+    for(var x= 0; x< 20; x++){
+        if(x%5==0 && i%5==0){
+        setColor(i,x,"red");
+        }
+        else if(x%2==0 && i%2==0){
+             setColor(i,x,"blue");
+        }
+    }
+}
